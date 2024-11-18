a4fbdafac7791a4c948001b5cbc0c5ff59b06613
Yaroslav Kishchuk <98614059+kshchuk@users.noreply.github.com>
2024-11-18 02:38:10 +0200

    added k6->influxdb->grafana tests

---
+++ k6-scripts/test.js
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+
+export const options = {
+    scenarios: {
+        constant_load: {
+            executor: 'constant-vus',
+            vus: 10,
+            duration: '30s',
+        },
+        ramping_load: {
+            executor: 'ramping-vus',
+            startVUs: 1,
+            stages: [
+                { duration: '10s', target: 10 },
+                { duration: '20s', target: 50 },
+                { duration: '10s', target: 0 },
+            ],
+            gracefulRampDown: '5s',
+        },
+        constant_rate: {
+            executor: 'constant-arrival-rate',
+            rate: 20,
+            timeUnit: '1s',
+            duration: '30s',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+        },
+    },
+};
+
+export default function () {
+    const res = http.get('http://springApp:8080/products');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'body contains products': (r) => r.body.includes('testName'),
+    });
+
+    // Think time
+    sleep(Math.random() * 2 + 1); // Випадковий час очікування від 1 до 3 секунд
+}
